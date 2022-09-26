84fffa874a94c811c4dfe249a3a63d295259c871
Ralcaraz96 <114370156+Ralcaraz96@users.noreply.github.com>
2022-09-26 10:54:43 -0500

    Code cleanup.
    Code reformat

--- AutomationCucumber/src/test/java/utils/TestBase.java
+++ AutomationCucumber/src/test/java/utils/TestBase.java
@@ -1,12 +1,15 @@
 package utils;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.Properties;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+
 public class TestBase {
     public WebDriver driver;
+
     public WebDriver WebDriverManager() throws IOException {
         FileInputStream fis = new FileInputStream(System.getProperty("user.dir") + "//src//test//resources//global.properties");
         Properties prop = new Properties();
